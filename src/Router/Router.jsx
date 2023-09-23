d337a09a52db3afdbca6e7898b321fccd18567af
NF RIDOY <nfridoy@gmail.com>
2023-09-24 03:21:06 +0600

    login card

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,6 +4,7 @@ import {
 import MainLayout from "../Layout/MainLayout";
 import Home from "../pages/home/Home";
 import Favorites from "../pages/favorites/Favorites";
+import Login from "../pages/login/Login";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -19,7 +20,7 @@ const router = createBrowserRouter([
             },
             {
                 path: "/login",
-                element: <div>Login</div>
+                element: <Login></Login>
             }
         ]
     },
